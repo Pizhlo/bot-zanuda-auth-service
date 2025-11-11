55af434023ee44889b3c9afbca3d2bb86e66720b
maria <zaush@bk.ru>
2025-11-11 18:45:42 +0300

    BZ-98: добавлен butler

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	//nolint:forbidigo // временно, пока не реализована функция
-	fmt.Println("Hello, World!")
+	ctx := context.Background()
+
+	butler := NewButler()
+
+	logrus.WithFields(logrus.Fields{
+		"version": butler.BuildInfo.Version,
+		"commit":  butler.BuildInfo.GitCommit,
+		"date":    butler.BuildInfo.BuildDate,
+	}).Info("starting service")
+	defer logrus.Info("shutdown")
+
+	notifyCtx, notify := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	defer notify()
+
+	logrus.Info("all services started")
+
+	// Ждем сигнал завершения
+	<-notifyCtx.Done()
+	logrus.Info("received shutdown signal, stopping services...")
+
+	// Ждем завершения всех горутин
+	butler.waitForAll()
+	logrus.Info("all services stopped")
 }
