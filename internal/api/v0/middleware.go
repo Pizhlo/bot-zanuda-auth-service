a8af70c6e5dbb1101e9ce54192356ca26d39fef8
maria <zaush@bk.ru>
2026-06-25 19:09:32 +0300

    BZ-170: рефакторинг слоя хендлер

--- internal/api/v0/middleware.go
+++ internal/api/v0/middleware.go
@@ -6,6 +6,7 @@ import (
 	"auth-service/pkg/audit"
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"slices"
 
@@ -67,16 +68,18 @@ func (s *MiddlewareHandler) CheckToken(next echo.HandlerFunc) echo.HandlerFunc {
 
 		token, err := s.authService.CheckToken(ctx, tokenHeader)
 		if err != nil {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"error": err.Error()})
+			logrus.WithError(err).Error("error checking token")
+			return errResponse(c, http.StatusUnauthorized, err)
 		}
 
 		payload, ok := s.authService.GetPayload(token)
 		if !ok {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid token: no payload"})
+			logrus.Error("invalid token: no payload")
+			return errResponse(c, http.StatusUnauthorized, errors.New("invalid token: no payload"))
 		}
 
 		if code, err := s.verifyPayload(c.Request().Context(), payload); err != nil {
-			return c.JSON(code, map[string]string{"error": err.Error()})
+			return errResponse(c, code, err)
 		}
 
 		logrus.WithFields(logrus.Fields{
@@ -90,7 +93,7 @@ func (s *MiddlewareHandler) CheckToken(next echo.HandlerFunc) echo.HandlerFunc {
 
 		userID := c.Request().Header.Get(userIDHeader)
 		if userID == "" {
-			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "invalid request: header 'X-Telegram-User-Id' not found"})
+			return errResponse(c, http.StatusUnauthorized, fmt.Errorf("invalid request: header '%s' not found", userIDHeader))
 		}
 
 		ctx = withUserID(c.Request().Context(), userID)
